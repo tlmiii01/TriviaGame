b66fe1d463f59f4c7b3a287a245a92e6ae05dcd4
Thomas Martin <tmartin.dev101010@gmail.com>
2018-08-27 22:11:03 -0600

    Added additional questions to the game.  Added timer functionality to game.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,23 @@
 var currentQuestion;
 var correctAnswer = "";
 var selectedQuestions = [];
+var maxQuestions = 10;
+var rightAnswerCount = 0;
+var wrongAnswerCount = 0;
+var maxTimer = 30;
+var currentTimer;
+var intervalID;
+
+function decrementCounter() {
+    currentTimer--;
+    $("#timer").html("</p>Time remaining: " + currentTimer + "</p>");
+
+    if (currentTimer === 0) {
+        clearInterval(intervalID);
+        $("#status").html("</p>Time's Up!!!</p>");
+        displayCorrectAnswerOnMiss();
+    }
+}
 
 function loadCurrentQuestion() {
     $("#question").html("<p>" + currentQuestion.question + "</p>");
@@ -14,13 +31,16 @@ function loadCurrentQuestion() {
             correctAnswer = currentQuestion.answers[i].answer;
         }
     }
+
+    // Turn on the timer
+    currentTimer = maxTimer;
+    intervalID = setInterval(decrementCounter, 1000);
 }
 
 function selectNextQuestion() {
     var questionNumber;
     var isNewQuestion = false;
     do {
-        debugger;
         questionNumber = Math.floor(Math.random() * questions.length);
         if (selectedQuestions.indexOf(questionNumber) === -1 ) {
             selectedQuestions.push(questionNumber);
@@ -31,20 +51,24 @@ function selectNextQuestion() {
     return questions[questionNumber];
 }
 
-
+function displayCorrectAnswerOnMiss() {
+    $("#status").append("<p>Correct answer is: " + correctAnswer + ".</p>");
+    wrongAnswerCount++;
+}
 
 $(document).ready( function() {
     currentQuestion = selectNextQuestion();
     loadCurrentQuestion();
 
     $(".answer-list .answers").on("click", function() {
+        // Stop the timer
+        clearInterval(intervalID);
         if ( $(this).hasClass("correct") ) {
             $("#status").html("<p>Correct Answer!!!</p>");
+            rightAnswerCount++;
         } else {
             $("#status").html("<p>Wrong Answer!!!</p>");
-
-            // need to find the right answer
-           $("#status").append("<p>Correct answer is: " + correctAnswer + ".</p>");
+            displayCorrectAnswerOnMiss();
         }
     })
 })
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -12,18 +12,95 @@ class Question {
     }
 }
 
-var questions = [ new Question("What is thy name?",
-    new Answer("Arthur, King of the Britians", true),
+var questions = [ new Question("What did the Knights who say Ni interested in?",
+    new Answer("A shrubbery", true),
+    new Answer("Artichokes", false),
+    new Answer("Polar Bears", false),
+    new Answer("Inquisitions", false)),
+
+    new Question("Who thought Camelot was \"a silly place?\"",
     new Answer("Sir Robin", false),
-    new Answer("Sir Lancelot the Brave", false),
-    new Answer("Sir Galahad", false)),
-
-    new Question("What is your favorite color?",
-    new Answer("Blue", false),
-    new Answer("Yellow", false),
-    new Answer("Red", false),
-    new Answer("I don't know that...", true))
-
+    new Answer("Prince Herbert", false),
+    new Answer("Sir Lancelot", false),
+    new Answer("King Arthur", true)),
+    
+    new Question("Which relic was used to destroy the killer bunny?",
+    new Answer("The Dagger of Devinity", false),
+    new Answer("The Holy Hand Grenade", true),
+    new Answer("The Papal Pen of Death", false),
+    new Answer("The Spiritual Spike", false)),
+    
+    new Question("She turned me into a ...?",
+    new Answer("Donkey!", false),
+    new Answer("Newt!", true),
+    new Answer("Horsefly!", false),
+    new Answer("Cabbage!", false)),
+    
+    new Question("Who lost both arms and boths his legs in a sword fight?",
+    new Answer("Sir Robin", false),
+    new Answer("The Black Knight", true),
+    new Answer("King Arthur", false),
+    new Answer("The Knight who said Ni", false)),
+    
+    new Question("What is the name of the bridge they must cross near the end of the movie?",
+    new Answer("The Bridge of Terror", false),
+    new Answer("The Bridge of Death", true),
+    new Answer("The Brodge of Hell", false),
+    new Answer("The Bridge of Doom", false)),
+    
+    new Question("What is the name of the deadly abyss they must cross?",
+    new Answer("The Valley of Neverending Torture", false),
+    new Answer("The Gorge of Eternal Peril", true),
+    new Answer("The Arroyo of Nightmarish Fatigue", false),
+    new Answer("The Rift of Heinous Flatulance", false)),
+    
+    new Question("Who give Arther the quest to seek the Holy Grail",
+    new Answer("Merlin", false),
+    new Answer("The Knight who said Ni", false),
+    new Answer("The Lady of the Lake", false),
+    new Answer("God", true)),
+    
+    new Question("What does Arthur do to try and gail access to the French castle?",
+    new Answer("Build a wooden badger", false),
+    new Answer("Say \"Ni\"", false),
+    new Answer("Dress as women", false),
+    new Answer("Build a wooden rabbit", true)),
+    
+    new Question("What does Arthur do to try and gain access to the French castle?",
+    new Answer("Build a wooden badger", false),
+    new Answer("Say \"Ni\"", false),
+    new Answer("Dress as women", false),
+    new Answer("Build a wooden rabbit", true)),
+    
+    new Question("How does Arhur and his Knights get past the Black Beast of Aaaargh?",
+    new Answer("Taunting", false),
+    new Answer("They ran away", false),
+    new Answer("THe Holy Hand Grenade of Antioch", false),
+    new Answer("Animator suffered a sudden fatal heart attack", true)),
+    
+    new Question("What does Arthur do to try and gain access to the French castle?",
+    new Answer("Build a wooden badger", false),
+    new Answer("Say \"Ni\"", false),
+    new Answer("Dress as women", false),
+    new Answer("Build a wooden rabbit", true)),
+    
+    new Question("How does one cross the Bridge of Death successfully??",
+    new Answer("Correctly answer five questions", false),
+    new Answer("Correctly answer three questions", true),
+    new Answer("Defeat the Black Knight", false),
+    new Answer("Offering up a shrubbery", false)),
+    
+    new Question("Do King Arthur and his knights ever find the Holy Grail??",
+    new Answer("Yes, and they set an example for others.", false),
+    new Answer("No, they get arrested at the end of the movie.", true),
+    new Answer("No, they only found a model of it.", false),
+    new Answer("Yes, but the swallows are carrying it away.", false)),
+    
+    new Question("Help! I'm being ...",
+    new Answer("robbed", false),
+    new Answer("repressed", true),
+    new Answer("taunted by a weird Frenchman", false),
+    new Answer("eaten", false))
 ];
 
 // var questions = new Question("What is thy name?",
